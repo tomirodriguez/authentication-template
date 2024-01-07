433c854f077508f945aedc2e20214bd917af6112
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2024-01-07 16:26:02 -0300

    chore: add some annotations on how the admin action works

--- src/actions/admin.ts
+++ src/actions/admin.ts
@@ -2,6 +2,12 @@
 
 import { currentUser } from "@/lib/current-user";
 
+/**
+ *
+ * @description This is based on jwt.
+ * If you need to get a field that con change periodically, you should check de database.
+ * The token won't be updated until the user logs out and logs in again.
+ */
 export const adminApi = async () => {
   const { role } = await currentUser();
 
