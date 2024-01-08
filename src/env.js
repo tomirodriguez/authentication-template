2e1f2e9bf01aefb7d6c38e6add9e035f7d13ca6d
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2024-01-08 08:51:31 -0300

    refactor: minor refactor

--- src/env.js
+++ src/env.js
@@ -28,13 +28,13 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
-    GITHUB_CLIENT_ID: z.string().min(1),
-    GITHUB_CLIENT_SECRET: z.string().min(1),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
 
-    GOOGLE_CLIENT_ID: z.string().min(1),
-    GOOGLE_CLIENT_SECRET: z.string().min(1),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
 
-    RESEND_API_KEY: z.string().min(1),
+    RESEND_API_KEY: z.string(),
   },
 
   /**
--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,19 +1,39 @@
 import * as z from "zod";
 
-// TODO: Unified repeated schemas
-
 export const UserRoleSchema = z.enum(["ADMIN", "USER"]);
 
 export type TUserRole = z.infer<typeof UserRoleSchema>;
+
+const EmailSchema = z.string().email({
+  message: "Email is required",
+});
+
+const PasswordSchema = z.string().min(6, {
+  message: "Minimum 6 characters required",
+});
+
+export const RegisterSchema = z.object({
+  email: EmailSchema,
+  password: PasswordSchema,
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
+
+export const LoginSchema = z.object({
+  email: EmailSchema,
+  password: PasswordSchema,
+  code: z.optional(z.string()),
+});
 
 export const SettingsSchema = z
   .object({
     name: z.optional(z.string()),
     isTwoFactorEnabled: z.optional(z.boolean()),
     role: UserRoleSchema,
-    email: z.optional(z.string().email()),
-    password: z.optional(z.string().min(6)),
-    newPassword: z.optional(z.string().min(6)),
+    email: z.optional(EmailSchema),
+    password: PasswordSchema,
+    newPassword: PasswordSchema,
   })
   .refine(
     (data) => {
@@ -44,12 +64,8 @@ export const SettingsSchema = z
 
 export const NewPasswordSchema = z
   .object({
-    password: z.string().min(6, {
-      message: "Minimum of 6 characters required",
-    }),
-    passwordConfirmation: z.string().min(6, {
-      message: "Minimum of 6 characters required",
-    }),
+    password: PasswordSchema,
+    passwordConfirmation: PasswordSchema,
   })
   .refine(
     (data) => {
@@ -61,29 +77,5 @@ export const NewPasswordSchema = z
   );
 
 export const ResetSchema = z.object({
-  email: z.string().email({
-    message: "Email is required",
-  }),
-});
-
-export const LoginSchema = z.object({
-  email: z.string().email({
-    message: "Email is required",
-  }),
-  password: z.string().min(1, {
-    message: "Password is required",
-  }),
-  code: z.optional(z.string()),
-});
-
-export const RegisterSchema = z.object({
-  email: z.string().email({
-    message: "Email is required",
-  }),
-  password: z.string().min(6, {
-    message: "Minimum 6 characters required",
-  }),
-  name: z.string().min(1, {
-    message: "Name is required",
-  }),
+  email: EmailSchema,
 });
