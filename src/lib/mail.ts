49381adca326bc5f14bf8a4e9afc75c2120cefb9
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2024-01-09 09:23:18 -0300

    refactor: remove hardcoded base url

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,10 +1,11 @@
 import { Resend } from "resend";
 import { env } from "@/env";
+import { getBaseUrl } from "./utils";
 
 const resend = new Resend(env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `${getBaseUrl()}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
@@ -14,7 +15,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   });
 };
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `${getBaseUrl()}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
